e191bc74355dab3d9242b5676e5d7106b9cef6b1
sanal-11 <sanal.chelattu@gmail.com>
2025-06-01 20:47:08 +0530

    ldd external package

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -7,11 +7,11 @@
 $(info ==== aesd-assignments.mk IS PARSED ====)
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = aae024c78d0a932216f388028ba7316565c5edfd 
+AESD_ASSIGNMENTS_VERSION = 43b0eb80d7c617ac810838f9fb93ac76c638643b
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments7.git
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignment-7-Sanal-11.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 # AESD_ASSIGNMENTS_SITE = /home/win/workspace/coursera/assignment-1-Sanal-11
 # AESD_ASSIGNMENTS_SITE_METHOD = local

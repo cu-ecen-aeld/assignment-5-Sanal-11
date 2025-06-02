a6bb62d318b8ced46e649192bd45752796c5bd78
sanal-11 <sanal.chelattu@gmail.com>
2025-06-02 18:50:49 +0530

    kernelmodules, local success

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,33 +1,55 @@
 
 ##############################################################
 #
-# AESD-ASSIGNMENTS
+# LDD-ASSIGNMENTS
 #
 ##############################################################
-$(info ==== aesd-assignments.mk IS PARSED ====)
+$(info ==== ldd.mk IS PARSED ====)
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 43b0eb80d7c617ac810838f9fb93ac76c638643b
+LDD_VERSION = 43b0eb80d7c617ac810838f9fb93ac76c638643b
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignment-7-Sanal-11.git
-AESD_ASSIGNMENTS_SITE_METHOD = git
-# AESD_ASSIGNMENTS_SITE = /home/win/workspace/coursera/assignment-1-Sanal-11
-# AESD_ASSIGNMENTS_SITE_METHOD = local
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+# LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-Sanal-11.git
+# LDD_SITE_METHOD = git
+LDD_SITE = ~/workspace/coursera/assignment-7-Sanal-11 
+LDD_SITE_METHOD = local
+# LDD_GIT_SUBMODULES = YES
 
 MODULE_SUBDIRS = misc-modules scull
 
-# define AESD_ASSIGNMENTS_BUILD_CMDS
-# 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
-# endef
+define LDD_BUILD_CMDS
+	# $(MAKE) -C $(LINUX_DIR) -C $(@D)/misc-modules modules
+	# $(MAKE) -C $(LINUX_DIR)  -C $(@D)/scull modules
+	# $(MAKE) -C $(LINUX_DIR) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(TARGET_CROSS) M=$(@D)/misc-modules modules
+	# $(MAKE) -C $(LINUX_DIR) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(TARGET_CROSS) M=$(@D)/scull modules
+	$(MAKE) -C $(LINUX_DIR) \
+	    ARCH=$(KERNEL_ARCH) \
+	    CROSS_COMPILE=$(TARGET_CROSS) \
+	    M=$(@D)/misc-modules \
+	    EXTRA_CFLAGS="-I$(@D)/include" \
+	    modules
+	$(MAKE) -C $(LINUX_DIR) \
+	    ARCH=$(KERNEL_ARCH) \
+	    CROSS_COMPILE=$(TARGET_CROSS) \
+	    M=$(@D)/scull \
+	    EXTRA_CFLAGS="-I$(@D)/include" \
+	    modules
+
+
+endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+define LDD_INSTALL_TARGET_CMDS
+
+	# Install .ko kernel modules
+	$(INSTALL) -D -m 0644 $(@D)/misc-modules/*.ko $(TARGET_DIR)/lib/modules/
+	$(INSTALL) -D -m 0644 $(@D)/scull/*.ko $(TARGET_DIR)/lib/modules/
+
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/scull/module_unload $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
 endef
 
